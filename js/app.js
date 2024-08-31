7098e7bb46d30ed10df2a0b5c264031f759827bf
Hector <rankecillo@gmail.com>
2024-08-31 12:25:23 -0600

    Added display tracker stats

--- js/app.js
+++ js/app.js
@@ -4,15 +4,72 @@ class CalorieTracker {
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+
+    this._displayCaloriesLimit();
+    this._displayCaloriesTotal();
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();
   }
+
+  // Public Methods/API //
 
   addMeal(meal) {
     this._meals.push(meal);
     this._totalCalories += meal.calories;
+    this._render();
   }
   addWorkout(workout) {
     this._workouts.push(workout);
     this._totalCalories -= workout.calories;
+    this._render();
+  }
+
+  // Private Methods
+
+  _displayCaloriesTotal() {
+    const _totalCaloriesEl = document.getElementById('calories-total');
+    _totalCaloriesEl.innerHTML = this._totalCalories;
+  }
+  _displayCaloriesLimit() {
+    const calorieLimitEl = document.getElementById('calories-limit');
+    calorieLimitEl.innerHTML = this._calorieLimit;
+  }
+
+  _displayCaloriesConsumed() {
+    const caloriesConsumedEl = document.getElementById('calories-consumed');
+
+    const consumed = this._meals.reduce(
+      (total, meal) => total + meal.calories,
+      0
+    );
+
+    caloriesConsumedEl.innerHTML = consumed;
+  }
+  _displayCaloriesBurned() {
+    const caloriesBurnedEl = document.getElementById('calories-burned');
+
+    const burned = this._workouts.reduce(
+      (total, workout) => total + workout.calories,
+      0
+    );
+
+    caloriesBurnedEl.innerHTML = burned;
+  }
+
+  _displayCaloriesRemaining() {
+    const calorieRemainingEl = document.getElementById('calories-remaining');
+
+    const remaining = this._calorieLimit - this._totalCalories;
+
+    calorieRemainingEl.innerHTML = remaining;
+  }
+
+  _render() {
+    this._displayCaloriesTotal();
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();
   }
 }
 
@@ -23,3 +80,19 @@ class Meal {
     this.calories = calories;
   }
 }
+class Workout {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+const tracker = new CalorieTracker();
+
+const breakfast = new Meal('Breakfast', 400);
+
+tracker.addMeal(breakfast);
+
+const run = new Workout('Morning run', 320);
+tracker.addWorkout(run);
